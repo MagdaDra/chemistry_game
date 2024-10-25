3ad2dd336deb3d507ecfe94ced5248afa9141d1c
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-10-25 21:39:44 +0200

    Added 'you won' box

--- src/index.js
+++ src/index.js
@@ -1,14 +1,64 @@
 const cards = [
-    {name: 'cylinder miarowy', img: 'cylinder-miarowy.png'},
-    {name: 'kolba', img: 'kolba.png'},
-    {name: 'lejek', img: 'lejek.png'},
-    {name: 'mozdzierz', img: 'mozdzierz.png'},
-    {name: 'palnik', img: 'palnik.png'},
-    {name: 'pipeta', img: 'pipeta.png'},
-    {name: 'probowka', img: 'probowka.png'},
-    {name: 'rozdzielacz', img: 'rozdzielacz.png'},
-    {name: 'stojak', img: 'stojak.png'},
-    {name: 'szalka petriego', img:'szalka-petriego.png'},
-    {name: 'termometr', img: 'termometr.png'},
-    {name: 'zlewka', img: 'zlewka.png'}
-]
+	{ name: 'cylinder miarowy', img: 'cylinder-miarowy.png' },
+	{ name: 'kolba', img: 'kolba.png' },
+	{ name: 'lejek', img: 'lejek.png' },
+	{ name: 'mozdzierz', img: 'mozdzierz.png' },
+	{ name: 'palnik', img: 'palnik.png' },
+	{ name: 'pipeta', img: 'pipeta.png' },
+	{ name: 'probowka', img: 'probowka.png' },
+	{ name: 'rozdzielacz', img: 'rozdzielacz.png' },
+	{ name: 'stojak', img: 'stojak.png' },
+	{ name: 'szalka petriego', img: 'szalka-petriego.png' },
+	{ name: 'termometr', img: 'termometr.png' },
+	{ name: 'zlewka', img: 'zlewka' },
+	{ name: 'cylinder miarowy', img: 'cylinder-miarowy.png' },
+	{ name: 'kolba', img: 'kolba.png' },
+	{ name: 'lejek', img: 'lejek.png' },
+	{ name: 'mozdzierz', img: 'mozdzierz.png' },
+	{ name: 'palnik', img: 'palnik.png' },
+	{ name: 'pipeta', img: 'pipeta.png' },
+	{ name: 'probowka', img: 'probowka.png' },
+	{ name: 'rozdzielacz', img: 'rozdzielacz.png' },
+	{ name: 'stojak', img: 'stojak.png' },
+	{ name: 'szalka petriego', img: 'szalka-petriego.png' },
+	{ name: 'termometr', img: 'termometr.png' },
+	{ name: 'zlewka', img: 'zlewka' },
+];
+
+const memoryGame = new MemoryGame(cards);
+
+window.addEventListener('load', (event) => {
+	let html = '';
+
+	memoryGame.shuffleCards();
+
+	memoryGame.cards.forEach((pic) => {
+		html += `
+        <div class='card' data-card-name='${pic.name}'>
+            <div class='back' name='${pic.img}'></div>
+            <div class='front' style='background: url(img/${pic.img}) no-repeat'></div>
+        </div>
+        `;
+	});
+
+	//Add all divs do the HTML
+	document.querySelector('#memory-board').innerHTML = html;
+
+	//Bind the click event of each element to a function
+	document.querySelectorAll('.card').forEach((card) => {
+		card.addEventListener('click', () => {
+			if (memoryGame.pairsGuessed === 12) {
+				const youWonBox = document.createElement('div');
+				youWonBox.className = 'you-won';
+                const boxParagraph = document.createElement('p');
+                boxParagraph.innetHTML = 'Koniec gry!';
+                youWonBox.appendChild(boxParagraph);
+                document.body.appendChild(youWonBox);
+
+                return
+			} else {
+                
+            }
+		});
+	});
+});
