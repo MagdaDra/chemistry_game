a88a11b8849003fdc9e96bb500a033404b21f658
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-10-25 21:09:14 +0200

    Images added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const cards = [
+    {name: 'cylinder miarowy', img: 'cylinder-miarowy.png'},
+    {name: 'kolba', img: 'kolba.png'},
+    {name: 'lejek', img: 'lejek.png'},
+    {name: 'mozdzierz', img: 'mozdzierz.png'},
+    {name: 'palnik', img: 'palnik.png'},
+    {name: 'pipeta', img: 'pipeta.png'},
+    {name: 'probowka', img: 'probowka.png'},
+    {name: 'rozdzielacz', img: 'rozdzielacz.png'},
+    {name: 'stojak', img: 'stojak.png'},
+    {name: 'szalka petriego', img:'szalka-petriego.png'},
+    {name: 'termometr', img: 'termometr.png'},
+    {name: 'zlewka', img: 'zlewka.png'}
+]
